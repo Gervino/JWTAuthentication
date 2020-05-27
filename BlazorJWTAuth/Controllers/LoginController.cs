0bff0a2f2beccc415bb92ff7a9e22568b7e4f8cf
Gerve <kougervino@gmail.com>
2020-05-28 01:53:54 +0200

    200528 GK

---
+++ BlazorJWTAuth/Controllers/LoginController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorJWTAuth.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BlazorJWTAuth.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config; 
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login(string UserId="", string pass="")
+        {
+            UserModel login = new UserModel();
+            login.UserName = UserId;
+            login.Pass = pass;
+
+            IActionResult response = Unauthorized();
+
+            var user = AuThenticatUser(login);
+            if(user.UserName!= null)
+            {
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
+            }
+            return response;
+        }
+
+        private UserModel AuThenticatUser(UserModel login)
+        {
+            var user = new UserModel();
+            if(login.UserName == "gervino" && login.Pass == "123")
+            {
+                //For Demo I have used static user               
+                user = new UserModel { UserName = "Gervino", Email = "kougervino@gmail.com", Pass = "123", Role = "Admin" };
+            }
+            return user;
+        }
+
+        private string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Sub, userInfo.UserName),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Email, userInfo.Email),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Role, userInfo.Role)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(20),
+                signingCredentials: credentials);
+
+            var encodetoken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodetoken;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            try
+            {
+                return Ok(value);
+            }
+            catch(Exception)
+            {
+                return NotFound();
+            }
+
+        }
+    }
+}
